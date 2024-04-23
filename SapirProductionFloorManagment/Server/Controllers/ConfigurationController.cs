e11c24253cf8bae123c127dcc11ff6339c09263c
nikit <nikit@DESKTOP-10CMOF7>
2024-04-23 17:29:21 +0300

    Add controller to configuration

---
+++ SapirProductionFloorManagment/Server/Controllers/ConfigurationController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SapirProductionFloorManagment.Server.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ConfigurationController : Controller
+    {
+
+        [HttpPost]
+        public void GetNumberOne(int num)
+        {
+            Console.WriteLine(num);
+
+        }
+
+        
+    }
+}
