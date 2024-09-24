93f2e2036dc37eb03b1d9868d364a0d676a6ebd6
nikit <nikit@DESKTOP-10CMOF7>
2024-09-24 23:06:00 +0300

    refactoring WokPlans algorithm(uncompleated)

--- SapirProductionFloorManagment/Server/DbContexts/MainDbContext.cs
+++ SapirProductionFloorManagment/Server/DbContexts/MainDbContext.cs
@@ -23,7 +23,7 @@ namespace SapirProductionFloorManagment.Server
         public MainDbContext()
         {
          
-            var ConnectionString = @"Data Source=DESKTOP-10CMOF7\SQLEXPRESS;Initial Catalog=SapirProductionManagment;User ID=account;Password=3194murkin;Encrypt=False";
+            var ConnectionString = @"Data Source=DESKTOP-10CMOF7\SQLEXPRESS;Initial Catalog=SapirProductsManagment9;User ID=account;Password=3194murkin;Encrypt=False";
             _ConnectionString = ConnectionString; 
 
         }
