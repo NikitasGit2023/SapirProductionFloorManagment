cd89c2cf765a6b8d5027bfe81243d045a11c3c9b
nikit <nikit@DESKTOP-10CMOF7>
2024-10-07 14:18:50 +0300

    no desc

---
+++ SapirProductionFloorManagment/Server/Migrations/20241007081902_Mig-v4.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SapirProductionFloorManagment.Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class Migv4 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+             name: "FullName",
+             table: "Users",
+             newName: "UserName");
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
